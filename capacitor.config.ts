59a0fefec37b5671a3c49535d6d201c97c01eac5
Damian Tarnawsky <damian@ionic.io>
2021-11-15 10:27:58 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cs-plex-170',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
